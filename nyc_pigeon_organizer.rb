18444605a62687f23f131098166b5c829505c808
github username <github email address>
2019-01-12 22:23:47 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_obj = {}
+  data.map do |key,value|
+      value.map do |x,y|
+        y.collect do |name|
+          binding.pry
+          if new_obj[name][key]
+            new_obj[name][key]<<x.to_s
+          elsif new_obj[name]
+            new_obj[name][key]=[x.to_s]
+          else
+            new_obj[name]={key=>x.to_s}
+          end
+          end
+        end
+      end
+
+  new_obj
 end
