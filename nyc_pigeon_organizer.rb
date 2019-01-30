ab10cf4beadf34aae83450c978a65b8b2d61eced
Ethan Nam <ethan.s.nam@gmail.com>
2019-01-30 14:19:31 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #set up initial hash
+  pigeon_list = {}
+
+  #get all the pigeons
+  data.each do |attribute, value|
+  	value.each do |key, array|
+  		array.each do |name|
+  			if pigeon_list[name] != nil
+  					if pigeon_list[name][attribute] != nil
+  						pigeon_list[name][attribute].push(key.to_s)
+  					else
+  						pigeon_list[name][attribute] = [key.to_s]
+						end
+	  		else
+					pigeon_list[name] = {
+						attribute => [key.to_s]
+					}
+	  		end
+  		end
+  	end
+  end
+  pigeon_list
 end
